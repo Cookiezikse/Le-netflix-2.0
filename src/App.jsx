ccb50f7bd74237d71c26cd93b456ddb5f9e2bc44
Cookiezikse <88719371+Cookiezikse@users.noreply.github.com>
2022-12-07 19:39:38 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+import Navbar from './Navbar';
+import Results from './Results';
+import Movie from "./Movie";
+import Action from "./Action"
+
+
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Results></Results>
+      <Movie></Movie>
+      <Action></Action>
+      
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <div>
+            <div className="category">
+                <ul>
+                    <li>Acceuil</li>
+                    <li>Nouveauté</li>
+                    <li>Catégorie</li>
+
+                </ul>
+                
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
